80a19086272b56494cf424f0c52fdb7a597e8ff2
D36CHOI <40600306+d36choi@users.noreply.github.com>
2020-08-02 16:30:39 +0900

    Create 2293.cpp
    vector, sort 필요없음

---
+++ 2293/2293.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define MAX 100+1
+#define K_MAX 10000+1
+using namespace std;
+int num_cases[K_MAX] = {0};
+int main()
+{
+    int n,k;
+    vector<int> coins_val;
+    int val;
+    cin >> n >> k;
+    for(int i=0; i<n; i++)
+    {
+        cin >> val;
+        coins_val.push_back(val);  
+    }
+    sort(coins_val.begin(),coins_val.end());
+    num_cases[0] = 1;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=coins_val[i]; j<=k; j++)
+        {
+            num_cases[j] += num_cases[j-coins_val[i]];
+        }
+    }   
+    cout << num_cases[k];
+    return 0;
+
+}
